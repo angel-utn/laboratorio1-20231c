64f49b186f32e89afb1da7b6f4efcafa66d82318
Brian Lara <brian.lara@outlook.com.ar>
2023-04-01 10:59:32 -0300

    diferencia absoluta

---
+++ Semana 03/Sabados Virtual/diferencia abs v3.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+/// Leer el enunciado
+/**
+	Hacer un programa para ingresar por teclado dos números 
+	y luego informar por pantalla la diferencia absoluta.
+*/
+
+/// 10 3  => 7
+/// 3  10 => -7 *-1  => 7
+
+/**
+	Analizar los datos
+	
+	Datos de Entrada: 
+		primer numero, segundo numero
+
+	Datos de Salida:
+		diferencia
+
+	Operaciones:
+		diferencia = primer numero - segundo numero
+
+		diferencia = diferencia * -1 	=> diferencia < 0
+
+*/
+
+/**
+	int para numeros enteros (numeros sin la coma)
+	float para los reales (numeros con coma)
+	char para los caracteres ( letras, simbolos, numeros, etc ) 'A', '>', '1'
+	bool para los valores de verdad ( verdadero (true) y falso (false))
+	string para los valores alfanumericos (textos) "hola mundo"
+*/
+
+/**
+	PascalCase  -- Nombres de Clase (LAB2)
+	camelCase   -- nombres de variables y funciones
+	UPPERCASE   -- nombre de las constantes
+	lowercase   -- nombres de variables y funciones
+	snake_case  -- nombres de variables y funciones
+*/
+
+int main() {
+	/// declaracion
+	/**
+		TIPO nombre;
+	*/
+	float primerNumero;
+	float segundoNumero;
+	float diferencia;
+	
+	/// datos de entrada
+	cout << "Ingrese primer numero: ";
+	cin >> primerNumero;
+	
+	cout << "Ingrese segundo numero: ";
+	cin >> segundoNumero;	
+	
+	/// operaciones
+		
+	if(primerNumero > segundoNumero){
+		diferencia = primerNumero - segundoNumero;
+	}
+	else{
+		diferencia = segundoNumero - primerNumero;
+	}
+	
+	/// datos de salida
+	cout << "La diferencia es: " << diferencia << endl;
+	
+	
+	return 0;
+}
+
