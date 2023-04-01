0399ab8f5b16a0a6ac7abffe863e33a7e1219370
Brian Lara <brian.lara@outlook.com.ar>
2023-04-01 10:42:37 -0300

    diferencia

---
+++ Semana 03/Sabados Virtual/diferencia v2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/// declaracion
+	float primerNumero;
+	float segundoNumero;
+	
+	/// datos de entrada
+	cout << "Ingrese primer numero: ";
+	cin >> primerNumero;
+	
+	cout << "Ingrese segundo numero: ";
+	cin >> segundoNumero;	
+	
+	/// datos de salida
+	cout << "La diferencia es: " << (primerNumero - segundoNumero) << endl;
+	
+	
+	return 0;
+}
+
---
+++ Semana 03/Sabados Virtual/diferencia.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+/// Leer el enunciado
+/**
+	Hacer un programa para ingresar por teclado dos números 
+	y luego informar por pantalla la diferencia.
+*/
+
+/**
+	Analizar los datos
+	
+	Datos de Entrada: 
+		primer numero, segundo numero
+
+	Datos de Salida:
+		diferencia
+
+	Operaciones:
+		diferencia = primer numero - segundo numero
+
+*/
+
+/**
+	int para numeros enteros (numeros sin la coma)
+	float para los reales (numeros con coma)
+	char para los caracteres ( letras, simbolos, numeros, etc ) 'A', '>', '1'
+	bool para los valores de verdad ( verdadero (true) y falso (false))
+	string para los valores alfanumericos (textos) "hola mundo"
+*/
+
+/**
+	PascalCase  -- Nombres de Clase (LAB2)
+	camelCase   -- nombres de variables y funciones
+	UPPERCASE   -- nombre de las constantes
+	lowercase   -- nombres de variables y funciones
+	snake_case  -- nombres de variables y funciones
+*/
+
+int main() {
+	/// declaracion
+	/**
+		TIPO nombre;
+	*/
+	float primerNumero;
+	float segundoNumero;
+	float diferencia;
+	
+	/// datos de entrada
+	cout << "Ingrese primer numero: ";
+	cin >> primerNumero;
+	
+	cout << "Ingrese segundo numero: ";
+	cin >> segundoNumero;	
+	
+	/// operaciones
+	diferencia = primerNumero - segundoNumero;
+	
+	/// datos de salida
+	cout << "La diferencia es: " << diferencia << endl;
+	
+	
+	return 0;
+}
+
