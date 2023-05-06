4ddeb45140fe055be907a8c167ed1fcaa1558032
Brian Lara <brian.lara@outlook.com.ar>
2023-05-06 13:48:58 -0300

    Sabado 06/05

---
+++ Semana 08/Sabado/float.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float numero = 1.1f;
+	
+	if(numero == 1.1f){
+		cout << "Es igual" << endl;
+	}
+	else{
+		cout << "No es igual" << endl;
+	}
+	
+	return 0;
+}
+
