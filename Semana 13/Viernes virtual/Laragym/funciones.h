aacb01d8a20e01cca25afbe7861fe66f78b45ac7
lauravelez <laurafvelez@gmail.com>
2023-06-10 19:25:32 -0300

    Archivos de la reunión virtual - Viernes 9/6

---
+++ Semana 13/Viernes virtual/Laragym/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void mostrarMenu();
+void cargarDatos(int [][10]);
+void puntoA(int mat[][10], int clientes, int tipos);
+void convertirMinutos(int , int &, int &);
+void puntoB(int mat[][10], int, int);
+void puntoC(int mat[][10], int, int);
+
+#endif // FUNCIONES_H_INCLUDED
